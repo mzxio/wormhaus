b8079b9b0fc6a7f0c42e331dcb43e0b1ac38f72d
Bones <bones@mzxio.org>
2022-01-28 05:54:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+
+// smoochWofe is simple clicker increment
+// multiplied by temporary per-session boosters that expire when you refresh
+
+
+// make fire/water to access special verbs
+
+// fire is cooking
+// water is preparing
+
+// gather ingredients gives base materials for fire/water
+// hunt for treasures rolls the dice on special overworld items
+// consult the worms for random item fetch quests
+// explore the caves for special underworld items
+// tend worms ??
+
+
